7c2c2b8a9312d16070c98cdb9b2d73584e32f544
KJ Kim <msrndsy@gmail.com>
2022-11-11 05:02:30 +0900

    Fix bug & slowdown nodes

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -23,57 +23,85 @@ function init() {
   window.requestAnimationFrame(draw);
 }
 
-function draw_node(ctx, node) {
-  ctx.beginPath();
-  ctx.arc(node.x, node.y, node.mass, 0, 2 * Math.PI);
-  ctx.stroke();
+function draw_nodes(ctx, nodes) {
+  for (let i = 0; i < nodes.length; i++){
+    let node = nodes[i];
 
-  ctx.font = '10px serif';
-  ctx.fillText(node.name, node.x + 10, node.y - 10);
+    ctx.beginPath();
+    ctx.arc(node.x, node.y, node.mass, 0, 2 * Math.PI);
+    ctx.stroke();
+  
+    ctx.font = '10px serif';
+    ctx.fillText(node.name, node.x + 10, node.y - 10);
+  }
 }
 
 function calcpower(link) {
   dx = Math.abs(link.n1.x - link.n2.x);
-  dy = Math.abs(link.n2.y - link.n2.y);
-  
+  dy = Math.abs(link.n1.y - link.n2.y);
+
   dist = Math.sqrt(dx * dx + dy * dy);
 
-  return (dist - link.r) * 0.0001
+  return (dist - link.r) * 0.0001;
 }
 
-function calcVec(n1, n2) {
-  vx = l1.n2.x - l1.n1.x;
-  vy = l1.n2.y - l1.n1.y;
-  absvec = Math.sqrt(vx * vx + vy * vy);
+function updateVecs(links, nodes) {
+  for (let li = 0; li < links.length; li++){
+    let link = links[li];
+    let n1 = link.n1;
+    let n2 = link.n2;
 
-  power = calcpower(l1)
+    let vx = n2.x - n1.x;
+    let vy = n2.y - n1.y;
 
-  n1.vx += vx * power / absvec;
-  n1.vy += vy * power / absvec;
-
-  vx = l1.n1.x - l1.n2.x;
-  vy = l1.n1.y - l1.n2.y;
+    absvec = Math.sqrt(vx * vx + vy * vy);
   
-  n2.vx += vx * power / absvec;
-  n2.vy += vy * power / absvec;
+    power = calcpower(link)
+  
+    n1.vx += vx * power / absvec;
+    n1.vy += vy * power / absvec;
+  
+    vx *= -1;
+    vy *= -1;
+  
+    n2.vx += vx * power / absvec;
+    n2.vy += vy * power / absvec;
+  }
+
+  for (let i = 0; i < nodes.length; i++){
+    let node = nodes[i];
+    node.vx *= 0.999;
+    node.vy *= 0.999;
+  }
 }
 
-function moveNode(){
-  n1.x += n1.vx;
-  n1.y += n1.vy;
+function moveNodes(nodes) {
+  for (let i = 0; i < nodes.length; i++) {
+    let node = nodes[i];
+    node.x += node.vx;
+    node.y += node.vy;
 
-  n2.x += n2.vx;
-  n2.y += n2.vy;
+    if (node.x > 1280){
+      node.x = 1280;
+    } else if (node.x < 0){
+      node.x = 0;
+    }
+
+    if (node.y > 720){
+      node.y = 720;
+    } else if (node.y < 0){
+      node.y = 0;
+    }
+  }
 }
 
 function draw() {
   const ctx = document.getElementById('canvas').getContext('2d');
   ctx.clearRect(0, 0, 1280, 720); // clear canvas
 
-  draw_node(ctx, n1);
-  draw_node(ctx, n2);
-  calcVec(n1, n2);
-  moveNode();
+  draw_nodes(ctx, nodes);
+  updateVecs(links, nodes);
+  moveNodes(nodes);
 
   window.requestAnimationFrame(draw);
 }
@@ -81,5 +109,12 @@ function draw() {
 
 let n1 = new Node(500, 100, 'node1');
 let n2 = new Node(1000, 100, 'node2');
+let n3 = new Node(300, 300, 'node3')
+let nodes = [n1, n2, n3];
+
 let l1 = new Link(n1, n2, 100);
+let l2 = new Link(n3, n2, 100);
+let l3 = new Link(n1, n3, 100);
+let links = [l1, l2, l3];
+
 init();
